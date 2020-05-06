315b7da720fa054f4cad103ab42f324bc3f7e726
lumanskiy <splatt@splatt.ru>
2020-05-06 08:58:19 -0700

    init commit

---
+++ LiteClient/Client.cs
@@ -0,0 +1,61 @@
+﻿using LiteNetLib;
+using System;
+using System.Threading;
+
+namespace LiteClient
+{
+    class Client
+    {
+        static int Main(string[] args)
+        {
+            if(args.Length < 3)
+            {
+                Console.Write("Usage: LiteClient IP PORT NUM_CLIENTS:");
+                Console.WriteLine("Example:  LiteClient 127.0.0.1 9001 5");
+                return 1;
+            }
+
+            string ip = args[0];
+            int port = int.Parse(args[1]);
+            int numClients = int.Parse(args[2]);
+
+            NetManager[] clients = new NetManager[numClients];
+
+            for(int i=0; i<numClients;i++)
+            {
+                EventBasedNetListener listener = new EventBasedNetListener();
+                NetManager client = new NetManager(listener);
+                clients[i] = client;
+                client.Start();
+                client.Connect(ip, port, "SomeConnectionKey");
+
+                listener.NetworkReceiveEvent += (fromPeer, dataReader, deliveryMethod) =>
+                {
+                    Console.WriteLine("We got: {0}", dataReader.GetString(1024));
+                    dataReader.Recycle();
+                };
+            }
+
+            // Read data
+            while (!Console.KeyAvailable)
+            {
+                for (int i = 0; i < numClients; i++)
+                {
+                    var client = clients[i];
+                    client.PollEvents();
+                }
+
+                Thread.Sleep(15);
+            }
+
+            // Stop all clients
+            for (int i = 0; i < numClients; i++)
+            {
+                var client = clients[i];
+                client.Stop();
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ LiteServer/Server.cs
@@ -0,0 +1,74 @@
+﻿using LiteNetLib;
+using LiteNetLib.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace LiteServer
+{
+    class Server
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.Write("Usage: LiteServer 9001");
+                return 1;
+            }
+
+            int port = int.Parse(args[0]);
+
+
+            EventBasedNetListener listener = new EventBasedNetListener();
+            NetManager server = new NetManager(listener);
+            server.Start(port);
+
+            List<NetPeer> peers = new List<NetPeer>();
+
+            listener.ConnectionRequestEvent += request =>
+            {
+                request.Accept();
+            };
+
+            listener.PeerConnectedEvent += peer =>
+            {
+                peers.Add(peer);
+                Console.WriteLine("We got connection: {0}", peer.EndPoint); // Show peer ip
+                NetDataWriter writer = new NetDataWriter();                 // Create writer class
+                writer.Put("Hello client!");                                // Put some string
+                peer.Send(writer, DeliveryMethod.ReliableOrdered);             // Send with reliability
+            };
+
+            listener.PeerDisconnectedEvent += (peer, disconnectInfo) =>
+            {
+                Console.WriteLine($"Peer {peer.EndPoint} disconnected: {disconnectInfo.Reason}");
+                peers.Remove(peer);
+            };
+
+            ulong numTick = 0;
+            while (!Console.KeyAvailable)
+            {
+                server.PollEvents();
+
+                for (int numPeer = 0; numPeer < peers.Count; numPeer++)
+                {
+                    string message = $"Hello peer number! {numPeer} Tick number {numTick} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ";
+                    byte[] bytes = Encoding.UTF8.GetBytes(message);
+
+                    NetPeer peer = peers[numPeer];
+                    
+                    peer.Send(bytes, DeliveryMethod.Unreliable);
+                }
+
+                numTick++;
+
+                Thread.Sleep(33);
+            }
+
+            server.Stop();
+
+            return 0;
+        }
+    }
+}
